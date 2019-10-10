ab6a57fafd58bd9f10ccda3639ee8a86ad7ca890
Alejandro <josealejandroibarra@hotmail.com>
2019-10-09 23:20:30 -0600

    Now with basic AR motion capture.

--- MotorTherapy/ViewController.swift
+++ MotorTherapy/ViewController.swift
@@ -8,18 +8,78 @@
 
 import UIKit
 import RealityKit
+import ARKit
+import Combine
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate {
+
     
     @IBOutlet var arView: ARView!
+    @IBOutlet weak var messageLabel: MessageLabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // The 3D character to display.
+    var character: BodyTrackedEntity?
+    let characterOffset: SIMD3<Float> = [0, 0, 0] // Offset the character by one meter to the left
+    let characterAnchor = AnchorEntity()
+    
+    // A tracked raycast which is used to place the character accurately
+    // in the scene wherever the user taps.
+    var placementRaycast: ARTrackedRaycast?
+    var tapPlacementAnchor: AnchorEntity?
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        arView.session.delegate = self
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        // If the iOS device doesn't support body tracking, raise a developer error for
+        // this unhandled case.
+        guard ARBodyTrackingConfiguration.isSupported else {
+            fatalError("This feature is only supported on devices with an A12 chip")
+        }
+
+        // Run a body tracking configration.
+        let configuration = ARBodyTrackingConfiguration()
+        arView.session.run(configuration)
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        arView.scene.addAnchor(characterAnchor)
+        
+        // Asynchronously load the 3D character.
+        var cancellable: AnyCancellable? = nil
+        cancellable = Entity.loadBodyTrackedAsync(named: "models/robot").sink(
+            receiveCompletion: { completion in
+                if case let .failure(error) = completion {
+                    print("Error: Unable to load model: \(error.localizedDescription)")
+                }
+                cancellable?.cancel()
+        }, receiveValue: { (character: Entity) in
+            if let character = character as? BodyTrackedEntity {
+                // Scale the character to human size
+                character.scale = [1.0, 1.0, 1.0]
+                self.character = character
+                cancellable?.cancel()
+            } else {
+                print("Error: Unable to load model as BodyTrackedEntity")
+            }
+        })
+    }
+    
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        for anchor in anchors {
+            guard let bodyAnchor = anchor as? ARBodyAnchor else { continue }
+            
+            // Update the position of the character anchor's position.
+            let bodyPosition = simd_make_float3(bodyAnchor.transform.columns.3)
+            characterAnchor.position = bodyPosition + characterOffset
+            // Also copy over the rotation of the body anchor, because the skeleton's pose
+            // in the world is relative to the body anchor's rotation.
+            characterAnchor.orientation = Transform(matrix: bodyAnchor.transform).rotation
+   
+            if let character = character, character.parent == nil {
+                // Attach the character to its anchor as soon as
+                // 1. the body anchor was detected and
+                // 2. the character was loaded.
+                characterAnchor.addChild(character)
+            }
+        }
     }
 }
