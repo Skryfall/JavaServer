40c719c2caa1a129cfa6dc458ff8e002641128b2
Alejandro <josealejandroibarra@hotmail.com>
2019-10-21 20:06:06 -0600

    Now with new work

---
+++ SwiftUI/MotorTherapy/MotorView.swift
@@ -0,0 +1,17 @@
+//
+//  MotorView.swift
+//  MotorTherapy
+//
+//  Created by Alejandro Ibarra on 10/11/19.
+//  Copyright © 2019 Schlafenhase. All rights reserved.
+//
+
+import RealityKit
+import ARKit
+import Combine
+
+class MotorView: ARView, ARSessionDelegate {
+    
+    
+    
+}
